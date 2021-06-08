618419090b82182527496573cc9ae6aebee4bd98
ChiralSquid <andrew.liang2001@gmail.com>
2021-06-08 14:01:54 +0100

    2d search maybe
    + importseaborn
    + 2d search nested for loop
    + some janky 3D plot of A, lamb vs chi^2

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import STOM_higgs_tools
 import matplotlib.pyplot as plt
 import numpy as np
+import seaborn as sns
+from mpl_toolkits import mplot3d
 
 vals = STOM_higgs_tools.generate_data()
 
@@ -10,7 +12,7 @@ bin_heights, bin_edges = np.histogram(vals, range=[104, 155], bins=30)
 bin_centres = (bin_edges[:-1] + bin_edges[1:]) / 2.
 plt.errorbar(bin_centres, bin_heights, np.sqrt(bin_heights), fmt=',', capsize=2)
 
-plt.xlabel("$m_{\gamma\gamma} (MeV)$")
+plt.xlabel("$m_{\gamma\gamma} (MeV)$")  # Latex syntax
 plt.ylabel("Number of entries")
 # plt.show()
 
@@ -18,20 +20,23 @@ plt.ylabel("Number of entries")
 # %% Background parametrisation
 import scipy.integrate as integrate
 import scipy
-from scipy.optimize import curve_fit
+import scipy.optimize
 
 background_data = [j for j in vals if j < 120]  # to avoid taking the signal bump, upper limit of 120 MeV set
 # print(background_data)
 N_background = len(background_data)
 print('the number of data points in the background is', N_background)
-sigma_background_data = sum(background_data)
-print('the sum of the data points in the background is', sigma_background_data)
-lamb = sigma_background_data / (N_background)  # maximum likelihood estimator for lambda
+
+sum_background_data = sum(background_data)
+print('the sum of the data points in the background is', sum_background_data)
+
+lamb = sum_background_data / N_background  # maximum likelihood estimator for lambda
 print('lambda estimate is', lamb)
 
 background_bin_edges = [k for k in bin_edges if
                         103 < k < 120]  # takes the histogram values only within the background data region
 background_bin_heights = bin_heights[0:9]  # As above, only takes the bin_heights within the background data region
+
 area_hist = sum(np.diff(
     background_bin_edges) * background_bin_heights)  # calculates the area under the histogram between 104MeV and 120MeV
 print('The area under the histogram is', area_hist)
@@ -40,8 +45,7 @@ A = area_hist / (lamb * (np.exp(-104 / lamb) - np.exp(
     -119.3 / lamb)))  # Finds a value for A, after having equated area_hist with the integral of B(x)
 print('A is', A)
 
-
-# data_area= integrate.cumtrapz(background_data, x=None, dx=1.0, initial=0.0)
+# data_area = integrate.cumtrapz(background_data, x=None, dx=1.0, initial=0.0)
 # print(data_area)
 
 # popt, pcov = curve_fit(get_B_expectation, bin_centres, bin_edges)
@@ -49,17 +53,30 @@ print('A is', A)
 B_x = STOM_higgs_tools.get_B_expectation(bin_edges, A, lamb)
 print(B_x)
 plt.plot(bin_edges, B_x, label='B(x)')
+plt.grid()
 plt.legend()
+plt.show()
+# %%
+a_values = []
+lamb_values = []
+chi_squared = []
+
+for l in np.arange(26, 29, 0.1):
+    for a in range(70000, 80000, 1000):
+        result = STOM_higgs_tools.get_B_chi(vals, [104, 119.3], 9, a, l)
+        chi_squared.append(result)
+        lamb_values.append(l)
+        a_values.append(a)
+
+plt.plot(a_values[0:30], chi_squared[0:30])
+fig = plt.figure()
+
+sns.set(style="darkgrid")
+ax = plt.axes(projection="3d")
+ax.scatter3D(a_values, lamb_values, chi_squared)
 plt.show()
 
 
-#%% Background Parameterisation
+#%%
 
-chi_vals = []
-
-for val in vals:
-    chi_val = stom.get_B_chi(val,(104,119.3),30,A,lamb)
-    chi_vals.append(chi_val)
-    next
-    
-print(chi_vals)
+print(STOM_higgs_tools.get_B_chi(vals, [104, 119.3], 9, A, lamb))
