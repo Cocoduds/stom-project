45578feae3161b58fe3f6085b983b727df904336
ChiralSquid <andrew.liang2001@gmail.com>
2021-06-12 16:10:47 +0100

    Added 2D search
    And also other tidbits

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 import STOM_higgs_tools
 import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
 import numpy as np
+import seaborn as sns
 
 vals = STOM_higgs_tools.generate_data()
-
+#%%
+sns.set_style("ticks")
 # bin_heights, bin_edges, patches = plt.hist(vals, range=[104, 155], bins=30)
 
 bin_heights, bin_edges = np.histogram(vals, range=[104, 155], bins=30)
 bin_centres = (bin_edges[:-1] + bin_edges[1:]) / 2.
+sns.scatterplot(x=bin_centres, y=bin_heights, s=10)
 plt.errorbar(bin_centres, bin_heights, np.sqrt(bin_heights), fmt=',', capsize=2)
 
-plt.xlabel("$m_{\gamma\gamma} (GeV)$")
-plt.ylabel("Number of entries")
 plt.xlabel("$m_{\gamma\gamma} (MeV)$")
 plt.ylabel("Number of entries")
 # plt.show()
 
-
-# %% Background parameterisation
 import scipy.integrate as integrate
 import scipy
 from scipy.optimize import curve_fit
@@ -49,21 +49,46 @@ print('A is', A)
 # popt, pcov = curve_fit(get_B_expectation, bin_centres, bin_edges)
 
 def get_B_expectation(xs, A, lamb):
-    ''' 
-    Return a set of expectation values for the background distribution for the 
-    passed in x values. 
-    '''
+    """
+    Return a set of expectation values for the background distribution for the
+    passed in x values.
+    """
     return [A * np.exp(-x / lamb) for x in xs]
 
 
-B_x = get_B_expectation(bin_edges, A, lamb)
+# B_x = get_B_expectation(bin_edges, A, lamb)
+B_x = get_B_expectation(bin_edges, 65000, 28.9)
+
 print(B_x)
-plt.plot(bin_edges, B_x, label='B(x)')
+plt.plot(bin_edges, B_x, label='B(x)', color='Black')
 plt.legend()
+sns.despine()
 
 plt.show()
+#%%
+"""Takes around 30s to run"""
+a_values = []
+lamb_values = []
+chi_squared = []
 
-# %%
+for l in np.arange(26, 29, 0.1):
+    for a in range(60000, 80000, 500):
+        result = STOM_higgs_tools.get_B_chi(vals, [104, 119.3], 9, a, l)
+        chi_squared.append(result)
+        lamb_values.append(l)
+        a_values.append(a)
+
+sns.set(style="darkgrid")
+ax = plt.axes(projection="3d")
+ax.scatter3D(a_values, lamb_values, chi_squared)
+plt.show()
+#%%
+"""Run this after previous cell"""
+i_chi2min = np.argmin(chi_squared)  # gives index where chi_squared is min.
+print(f"A = {a_values[i_chi2min]}, lamb = {lamb_values[i_chi2min]}, chi2 = {chi_squared[i_chi2min]}")
+print(STOM_higgs_tools.get_B_chi(vals, [104, 119.3], 9, A, lamb))
+
+#%%
 
 chi_value_background_only = STOM_higgs_tools.get_B_chi(background_data, (104, 119.3), 9, A,
                                                        lamb)  # it is 9 since we consider only before the bump
@@ -73,7 +98,7 @@ chi_value_with_signal = STOM_higgs_tools.get_B_chi(vals, (104, 155), 30, A, lamb
 print(chi_value_with_signal,
       'seeing as this is much higher than for the signal only hypothesis, that implies signla-only is not such a good idea')
 
-# %%
+#%%
 
 chi_valus = STOM_higgs_tools.get_B_chi(background_data, (104, 119.3), 9, A,
                                        lamb)  # it is 9 since we consider only before the bump
@@ -83,7 +108,7 @@ chi_value = STOM_higgs_tools.get_B_chi(vals, (104, 155), 30, A, lamb)
 print(chi_value,
       'seeing as this is much higher than for the signal only hypothesis, that implies signla-only is not such a good idea')
 
-# %%
+#%%
 
 
 from scipy.stats import chi2
@@ -94,7 +119,7 @@ print(p_value_background_only, 'implies we can reject the background only hypote
 p_value_with_signal = chi2.sf(chi_value, 1)
 print(p_value_with_signal, 'implies can be accepted at 97% confidence level')
 
-# %%
+#%%
 
 chi_vals = []
 
@@ -130,7 +155,7 @@ plt.errorbar(bin_centres, bin_heights, np.sqrt(bin_heights), fmt=',', capsize=2)
 plt.show()
 
 
-# %%
+#%%
 def fit_func(x):
     gaus = 700 * np.exp(-(x - 125) ** 2 / (2 * 1.5 ** 2))
     expo = A * np.exp(-x / lamb)
@@ -174,7 +199,7 @@ chi_value_gaussfit = get_B_chi_gaussian(vals, (104, 155), 30)
 print(chi_value_gaussfit,
       'seeing as this is much higher than for the signal only hypothesis, that implies signla-only is not such a good idea')
 
-# %%
+#%%
 from scipy.optimize import curve_fit
 
 
@@ -212,9 +237,9 @@ def get_B_chi_gaussian_optimal(vals, mass_range, nbins):
 
 chi_value_gaussfit_optimal = get_B_chi_gaussian_optimal(vals, (104, 155), 30)
 print(chi_value_gaussfit_optimal)
+plt.show()
 
-
-# %%
+#%%
 
 def for_looping_chis_gauss(x, mu):
     gaus = fit[0] * np.exp(-(x - mu) ** 2 / (2 * fit[2] ** 2))
